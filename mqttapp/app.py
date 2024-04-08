16aeb5ef1e028b48adbc12b84d4d6685df9769f4
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2024-04-08 23:05:01 +0200

    add availability_topic

--- mqttapp/app.py
+++ mqttapp/app.py
@@ -9,15 +9,17 @@ class App:
         self.logger = get_logger('App', level=app_config.log_level)
         self.logger.debug('config: {}'.format(app_config))
         self.component = app_config.component
+        self.availability_topic = '{}/status'.format(self.component)
         self.logger.info('started')
         self.loop = asyncio.get_event_loop()
-        self.client = config.get_mqtt_client()
+        self.client = config.get_mqtt_client(component=self.component, will_topic=self.availability_topic)
         self.loop.run_until_complete(self._main())
 
     async def _main(self):
         await asyncio.sleep(1)
         async with self.client:
             self.logger.info('connected to broker.')
+            await self.client.publish(topic=self.availability_topic, payload='online', qos=2, retain=True)
             await self.initialize()
             async for message in self.client.messages:
                 self.logger.debug('mqtt message received - {}: {}'.format(message.topic, message.payload.decode()))
--- mqttapp/config.py
+++ mqttapp/config.py
@@ -11,11 +11,13 @@ def get_config() -> Dict[str, Any]:
     return parser.parse_args()
 
 
-def get_mqtt_client() -> aiomqtt.Client:
+def get_mqtt_client(component: str, will_topic: str) -> aiomqtt.Client:
+    will = aiomqtt.Will(topic=will_topic, payload='offline', qos=2, retain=True)
     return aiomqtt.Client(
         os.getenv('MQTT_HOSTNAME', '127.0.0.1'),
         port=int(os.getenv('MQTT_PORT', '1883')),
         username=os.getenv('MQTT_USERNAME', None),
         password=os.getenv('MQTT_PASSWORD', None),
         identifier=os.getenv('MQTT_IDENTIFIER', 'mqttapp'),
+        will=will,
     )
