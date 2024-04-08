ec3f41436126cab9eb5c9a145d175d67703db486
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2024-04-08 23:09:52 +0200

    refactor app import

--- mqttapp/app.py
+++ mqttapp/app.py
@@ -1,18 +1,18 @@
 import asyncio
-from . import config
+from .config import get_config, get_mqtt_client
 from .log import get_logger
 
 
 class App:
     def __call__(self):
-        app_config = config.get_config()
-        self.logger = get_logger('App', level=app_config.log_level)
-        self.logger.debug('config: {}'.format(app_config))
-        self.component = app_config.component
+        config = get_config()
+        self.logger = get_logger('App', level=config.log_level)
+        self.logger.debug('config: {}'.format(config))
+        self.component = config.component
         self.availability_topic = '{}/status'.format(self.component)
         self.logger.info('started')
         self.loop = asyncio.get_event_loop()
-        self.client = config.get_mqtt_client(component=self.component, will_topic=self.availability_topic)
+        self.client = get_mqtt_client(component=self.component, will_topic=self.availability_topic)
         self.loop.run_until_complete(self._main())
 
     async def _main(self):
